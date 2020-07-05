937a5f4ea999b5d56d390db59acfc9e3d5784997
svasandani <vasandanishailesh@gmail.com>
2020-07-05 02:16:14 -0400

    Initial commit for dynamic-tags

---
+++ dynamic-tags/script.js
@@ -0,0 +1,46 @@
+// var blog = new DynamicTagController({
+//   "container": "container",
+//   "card": "el",
+//   "tag": "tag",
+//   "filter": "filter",
+//   "dataTag": "tag"
+// })
+//
+// blog.loadTags();
+//
+// var dtc2 = new DynamicTagController({
+//   "container": "container2",
+//   "card": "el2",
+//   "tag": "tag2",
+//   "filter": "filter2",
+//   "noResult": "Ain't nuthin' here"
+// })
+//
+// dtc2.loadTags();
+
+function getScrollPercent() {
+  var h = document.documentElement,
+    b = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight';
+  return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+}
+
+function populateBackground() {
+  for (let i = 0; i < 1000; i++) {
+    let el = document.createElement("div");
+    el.classList.add("bg-box");
+    console.log(el);
+    el.style.width = Math.floor(Math.random() * 80) + 30 + "px";
+    bg.appendChild(el);
+  }
+}
+
+var bg = document.querySelector(".background");
+
+document.addEventListener('DOMContentLoaded', populateBackground);
+
+window.addEventListener('scroll', function() {
+  bg.style.transform = "translateY(-" + getScrollPercent() * 0.3 + "vh)";
+  console.log(bg.style.transform);
+});
